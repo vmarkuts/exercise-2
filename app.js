d1c8d7d88bad5a4cfa9b734ab93041337354b74b
vlad <vlad.markuts@gmail.com>
2020-04-20 17:52:33 +0300

    playing with post requests and faker.js

--- app.js
+++ app.js
@@ -1,7 +1,37 @@
 var express = require ('express');
 var app = express();
+var bodyParser = require ('body-parser');
+var faker = require ('faker');
+faker.locale = 'de';
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
+
+var friends = [];
+const defaultFriends = 10;
+
+renderFriends(defaultFriends);
+
+function renderFriends(num){
+	for (var i = 0; i < num; i++) {
+		var randomFullName = faker.name.prefix() + ' ' + faker.name.firstName() + ' ' + faker.name.lastName();
+		friends.push(randomFullName);
+	}
+}
+
+app.get('/', function(req,res){
+	res.render('home');
+});
+
+app.post('/addfriend', function(req,res){
+	var newFriend = req.body.newfriend;
+	friends.push(newFriend);
+	res.redirect('/friends');
+})
+
+app.get('/friends', function(req,res){
+	res.render('friends', {friends: friends});
+});
 
 app.listen(3000, function(){
 	console.log('server is running!');
